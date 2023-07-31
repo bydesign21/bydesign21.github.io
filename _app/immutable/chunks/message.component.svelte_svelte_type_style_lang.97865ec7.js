9792b3a988a3275da7bc76f6be8891721f651eeb
Logan Vasquez <loganvasquez@logans-mbp.lan>
2023-07-30 20:28:35 -0500

    Added social media icons to navigation, made it more resillient, added to theming

---
+++ _app/immutable/chunks/message.component.svelte_svelte_type_style_lang.97865ec7.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index.a66543d9.js";const t=e(null),o=e([]);export{o as m,t as s};
--- _app/immutable/chunks/message.component.svelte_svelte_type_style_lang.a4c74851.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index.a7e9618d.js";const t=e(null),o=e([]);export{o as m,t as s};
